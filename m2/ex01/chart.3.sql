4ba490f0cce6f591259f6a00cbf963cda2c06a1a
slaye <uduterrage@gmail.com>
2025-07-22 17:31:52 +0200

    todo: fix sql request

--- m2/ex01/chart.3.sql
+++ m2/ex01/chart.3.sql
@@ -1,3 +1,19 @@
-SELECT DATE(event_time), AVG(price) FROM customers
-WHERE event_type = 'purchase'
-GROUP BY DATE(event_time);
+SELECT 
+    day,
+    AVG(avg_spend) AS avg_spend_per_user
+FROM (
+    SELECT 
+        DATE(event_time) AS day,
+        user_id,
+        AVG(price) AS avg_spend
+    FROM 
+        customers
+    WHERE 
+        event_type = 'purchase'
+    GROUP BY 
+        day, user_id
+) AS daily_user_avg
+GROUP BY 
+    day
+ORDER BY 
+    day;
