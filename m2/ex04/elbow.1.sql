b6ec3a5bfa5500b5d85454fe6c49c10399b21607
slaye <uduterrage@gmail.com>
2025-07-24 16:45:01 +0200

    todo: re-calculate k_center each time for each random k_points

--- m2/ex04/elbow.1.sql
+++ m2/ex04/elbow.1.sql
@@ -1,2 +1,3 @@
 SELECT SUM(CASE WHEN event_type = 'purchase' THEN 1 ELSE 0 END), SUM(price) FROM customers
+WHERE event_type = 'purchase'
 GROUP BY user_id;
--- m2/ex04/elbow.py
+++ m2/ex04/elbow.py
@@ -3,6 +3,7 @@ import sqlalchemy.orm as sqlaorm
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import random
 
 
 def read_sql_file(path: str) -> str:
@@ -12,8 +13,41 @@ def read_sql_file(path: str) -> str:
 	return data
 
 
+def k_means(data: list, k: int, states: int) -> list:
+	data_length = len(data)
+	result = []
+	for i in range(states):
+		state_result = {}
+		# Initialize the state result with empty lists for each k
+		for i in range(k):
+			state_result[i] = []
+		# Randomly select k points from the data
+		k_points_index = random.sample(range(data_length), k)
+		# Looping through each point in the data
+		for current_point_index in range(data_length):
+			distances = []
+			current_point = data[current_point_index]
+			# Calculate the distance from the current point to each of the k points
+			for k_point_index in k_points_index:
+				k_point = data[k_point_index]
+				# Euclidean distance calculation
+				distances.append(((k_point[0] - current_point[0]) ** 2+ (current_point[1] - k_point[1]) ** 2) ** 0.5)
+			# Append the current point to the kluster with the smallest distance
+			state_result[distances.index(min(distances))].append(current_point_index)
+		for i in range(k):
+			state_result[i] = len(state_result[i])
+		result.append(state_result)
+	return result
+
+
 def users_chart(data) -> None:
 	df = pd.DataFrame(data, columns=["purchases", "total_spent"])
+	for i in range(1, 11):
+		means = k_means(df.values.tolist(), i, 42)
+		for j in means:
+			print(f"state {means.index(j) + 1}: {j}")
+		print()
+	return
 	sns.relplot(
 		data=df,
 		x="purchases",
