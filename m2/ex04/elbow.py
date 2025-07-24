b214576c427baafe181a04ad1db883d52ee2484c
slaye <uduterrage@gmail.com>
2025-07-24 17:31:41 +0200

    feat: working users cluster graph (k_mean algorithm)

--- m2/ex04/elbow.py
+++ m2/ex04/elbow.py
@@ -13,45 +13,51 @@ def read_sql_file(path: str) -> str:
 	return data
 
 
-def k_means(data: list, k: int, states: int) -> list:
+def k_means(data: list, k: int, k_center) -> list:
 	data_length = len(data)
-	result = []
-	for i in range(states):
-		state_result = {}
+	result = {}
+	# Randomly select k points from the data
+	k_points = random.sample(range(data_length), k)
+	# Convert the indices to actual data points
+	for j in k_points:
+		k_points[k_points.index(j)] = data[j]
+	# Ajusting the k points to be unique and centered
+	for j in range(k_center):
 		# Initialize the state result with empty lists for each k
-		for i in range(k):
-			state_result[i] = []
-		# Randomly select k points from the data
-		k_points_index = random.sample(range(data_length), k)
+		for m in range(k):
+			result[m] = []
 		# Looping through each point in the data
 		for current_point_index in range(data_length):
 			distances = []
 			current_point = data[current_point_index]
 			# Calculate the distance from the current point to each of the k points
-			for k_point_index in k_points_index:
-				k_point = data[k_point_index]
+			for kluster in k_points:
 				# Euclidean distance calculation
-				distances.append(((k_point[0] - current_point[0]) ** 2+ (current_point[1] - k_point[1]) ** 2) ** 0.5)
+				distances.append(((kluster[0] - current_point[0]) ** 2 + (current_point[1] - kluster[1]) ** 2) ** 0.5)
 			# Append the current point to the kluster with the smallest distance
-			state_result[distances.index(min(distances))].append(current_point_index)
-		for i in range(k):
-			state_result[i] = len(state_result[i])
-		result.append(state_result)
+			result[distances.index(min(distances))].append(current_point_index)
+		# Calculate the new k points as the mean of the points in each kluster
+		for kluster_index in range(len(k_points)):
+			if len(result[kluster_index]) > 0:
+				mean_x = sum([data[index][0] for index in result[kluster_index]]) / len(result[kluster_index])
+				mean_y = sum([data[index][1] for index in result[kluster_index]]) / len(result[kluster_index])
+				k_points[kluster_index] = (mean_x, mean_y)
 	return result
 
 
 def users_chart(data) -> None:
 	df = pd.DataFrame(data, columns=["purchases", "total_spent"])
-	for i in range(1, 11):
-		means = k_means(df.values.tolist(), i, 42)
-		for j in means:
-			print(f"state {means.index(j) + 1}: {j}")
-		print()
-	return
+	means = k_means(df.values.tolist(), 3, 10)
+	labels = [None] * len(data)
+	for cluster_id, indices in means.items():
+		for idx in indices:
+			labels[idx] = cluster_id
+	df["cluster"] = labels
 	sns.relplot(
 		data=df,
 		x="purchases",
 		y="total_spent",
+		hue="cluster",
 		legend=False,
 	)
 	plt.xlabel("number of purchases")
