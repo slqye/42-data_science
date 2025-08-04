c1b91c9085cda2279ff262e29b7fe683ac666197
slaye <uduterrage@gmail.com>
2025-08-04 15:23:31 +0200

    feat: using argv

--- m4/ex04/Tree.py
+++ m4/ex04/Tree.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,7 +10,7 @@ from sklearn.tree import DecisionTreeClassifier
 DEFAULT_TRAIN_CSV_PATH = "../Train_knight.csv"
 
 
-def calc_variances(df: pd.DataFrame) -> None:
+def decision_tree(df: pd.DataFrame) -> None:
 	df["knight"] = df["knight"].apply(lambda x: 1 if x == "Sith" else 0)
 	df_normalized = (df - df.min()) / (df.max() - df.min())
 	features = ["Reactivity", "Push", "Survival", "Deflection"]
@@ -30,18 +31,19 @@ def check_default_csv_path(train_csv_path):
 	return train_csv_path
 
 
-def main():
+def main(argv: list[str]) -> None:
+	if len(argv) < 3:
+		print("error: usage: python Tree.py <train_csv_path> <test_csv_path>")
+		return
 	try:
 		sns.set_theme()
-		train_csv_path = check_default_csv_path(
-			str(input("path \"Train_knight.csv\": "))
-		)
-		train_df = pd.read_csv(train_csv_path)
-		calc_variances(train_df)
+		train_df = pd.read_csv(argv[1])
+		test_df = pd.read_csv(argv[2])
+		decision_tree(train_df)
 	except Exception as e:
 		print(f"error: {e}")
 		return
 
 
 if __name__ == "__main__":
-	main()
+	main(sys.argv)
