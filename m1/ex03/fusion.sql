a2e39a7d3cc8eca43b8d8f684ace91095a96ad19
slaye <uduterrage@gmail.com>
2025-08-07 16:19:22 +0200

    fix: ranking items by NULLs values

--- m1/ex03/fusion.sql
+++ m1/ex03/fusion.sql
@@ -5,12 +5,33 @@ ADD COLUMN IF NOT EXISTS category_id BIGINT,
 ADD COLUMN IF NOT EXISTS category_code TEXT,
 ADD COLUMN IF NOT EXISTS brand TEXT;
 
+WITH ranked_items AS (
+    SELECT
+        product_id,
+        category_id,
+        category_code,
+        brand,
+        ROW_NUMBER() OVER (
+            PARTITION BY product_id
+            ORDER BY 
+                (CASE WHEN category_id IS NOT NULL THEN 1 ELSE 0 END +
+                 CASE WHEN category_code IS NOT NULL THEN 1 ELSE 0 END +
+                 CASE WHEN brand IS NOT NULL THEN 1 ELSE 0 END) DESC
+        ) AS rn
+    FROM items
+),
+best_items AS (
+    SELECT *
+    FROM ranked_items
+    WHERE rn = 1
+)
+
 UPDATE customers
 SET
-	category_id = items.category_id,
-	category_code = items.category_code,
-	brand = items.brand
-FROM items
-WHERE items.product_id = customers.product_id;
+    category_id = best_items.category_id,
+    category_code = best_items.category_code,
+    brand = best_items.brand
+FROM best_items
+WHERE best_items.product_id = customers.product_id;
 
 COMMIT;
