fa9cc4f29646aeeb7e81308121f97fb99123b74c
slaye <uduterrage@gmail.com>
2025-08-13 09:34:55 +0200

    fix: split.py randomness

--- m3/ex05/split.py
+++ m3/ex05/split.py
@@ -1,12 +1,10 @@
 import pandas as pd
 import seaborn as sns
 
-
 DEFAULT_TRAIN_CSV_PATH = "../Train_knight.csv"
 
-
 def generate_files(train_df):
-	df_training = train_df.sample(n=int(train_df.shape[0] * 0.8), random_state=42)
+	df_training = train_df.sample(n=int(train_df.shape[0] * 0.8))
 	df_validation = train_df.drop(df_training.index)
 	df_training.to_csv("Training_knight.csv", index=False)
 	df_validation.to_csv("Validation_knight.csv", index=False)
@@ -16,7 +14,6 @@ def check_default_csv_path(train_csv_path):
 	if train_csv_path == "":
 		return DEFAULT_TRAIN_CSV_PATH
 	return train_csv_path
-
 
 def main():
 	try:
@@ -28,7 +25,6 @@ def main():
 		generate_files(train_df)
 	except Exception as e:
 		print(f"error: {e}")
-		return
 
 
 if __name__ == "__main__":
